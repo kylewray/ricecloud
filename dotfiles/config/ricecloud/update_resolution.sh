37b609d7cce3fe5055052b7f7ce8c164b5865a2d
Kyle Hollins Wray <kyle.hollins.wray@gmail.com>
2018-03-14 13:48:08 -0400

    Minor update to update_resolution script.

--- dotfiles/config/ricecloud/update_resolution.sh
+++ dotfiles/config/ricecloud/update_resolution.sh
@@ -3,21 +3,23 @@
 # Update the resolution and the displays.
 xrandr --output ${1} --mode ${3}x${4} --rate ${5} --primary --output ${2} --off
 
-# Note: "&" launches a child process, and "disown" essentially starts a grandchild
-# process for this shell script. When the child process dies, the grandchild is
-# inherited by init. This convenient syntactic technique is only usable in bash.
+# Now kill all processes we need to restart.
+killall polybar
 
 # Update the wallpaper, since the resolution likely changed.
-#killall nitrogen
-#disown -h `nitrogen --restore` &
-nitrogen --restore &
+nitrogen --restore
 
-# Update the polybar, since it needs to know the new dimensions and window.
-killall polybar
-disown -h `polybar ricecloud` &
+# After all updates, we pause a second to let everything close and update.
+sleep 1
 
-# TODO: Figure out why this works and "exit 0" does not...
-killall update_resolution.sh
+# Note: "&" launches a child process, and "setsid" detaches it from the controlling
+# tty. Then "(...)" in bash starts a grandchild process. When the child process dies,
+# the grandchild is inherited by init. This convenient syntactic technique is only
+# usable in bash.
+
+# Finally, we fork all the new processes we needed to relaunch.
+# This includes the polybar, since it needs to know the new dimensions and window.
+(setsid polybar ricecloud &)
 
 exit 0
 
